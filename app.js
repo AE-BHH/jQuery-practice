749e9513b7965f51c14871794bf95b8ba6c01962
AE/BHH <ehsaniabbas9@gmail.com>
2023-06-06 11:16:28 -0600

    use array of obj to get foods and color

--- app.js
+++ app.js
@@ -8,4 +8,34 @@ $(document).ready(function () {
 	siblingElement.text('This is the next part')
 	// appends the new div to startingElement
 	startingElement.after(siblingElement)
+
+	let favoriteFoods = $('#favorite-foods')
+	// favoriteFoods.css('background-color', 'purple')
+
+	// const favFoods = ['Bolani', 'Mantu', 'Qabli']
+	// const favColors = ['green', 'orange', '#fff']
+
+	// favFoods.forEach((food) => {
+	// 	let favFood = $('<div></div>').text(food)
+
+	// })
+
+	const favs = [
+		{ food: 'pizza', color: 'green' },
+		{ food: 'falafel', color: 'yellow' },
+		{ food: 'bolani', color: 'red' },
+		{ food: 'hot dog', color: 'orange' },
+	]
+	// how the array 'favs' looks like
+
+	// console.log(favs)
+
+	// access each elements in the array
+	favs.forEach((el) => {
+		let foodColors = $('<div></div>')
+			.text(el.food)
+			.css('background-color', el.color)
+
+		favoriteFoods.after(foodColors)
+	})
 })
