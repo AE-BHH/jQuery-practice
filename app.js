9c98ae86aa07706b5b59040ff91da26da404c31b
AE/BHH <ehsaniabbas9@gmail.com>
2023-06-06 09:06:19 -0600

    new div with contains added to the page by jQuery

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-	console.log('ready!')
+	// grabs the element with ID
+	const startingElement = $('#starting-point')
+
+	//creates a new div
+	const siblingElement = $('<div></div>')
+	// adds test to the new div
+	siblingElement.text('This is the next part')
+	// appends the new div to startingElement
+	startingElement.after(siblingElement)
 })
